f89b26fdf520c17f231466afa680004ddb559a67
Kerry Woodall <kerry.woodall@gmail.com>
2024-03-29 07:09:35 +0000

    add derivative

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ComponentB from './ComponentB.jsx';
 import ChainRuleExample from './ChainRuleExample.jsx';
 import React, { useState } from 'react';
 import IntegralGraph from './IntegralGraph.jsx';
+import Graph from './Graph.jsx';
 
 function CurrentDate() {
   const [date, setDate] = useState(new Date());
@@ -15,19 +16,26 @@ function CurrentDate() {
   );
 }
 function App() {
+
+  const graphData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    values: [65, 59, 80, 81, 56, 55, 40]
+  };
+
   return (
-    <div className="App">
+
+    <div className="App" >
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
         </p>
         <p className="small">
-          Kerry Woodall  <CurrentDate/>
+          Kerry Woodall  <CurrentDate />
         </p>
-      
-        <ChainRuleExample/>
-        <IntegralGraph/>
+
+        <ChainRuleExample />
+        <IntegralGraph />
 
         <ComponentA />
         <ComponentB />
@@ -38,3 +46,4 @@ function App() {
 }
 
 export default App;
+// <Graph data={graphData} />
