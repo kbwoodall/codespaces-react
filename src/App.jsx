fbc7b7021581a3cae1f3ae896eff09bc2a364c79
Kerry Woodall <kerry.woodall@gmail.com>
2024-08-15 02:31:23 +0000

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import LinearGraph from './ LinearGraph';
 import Dashboard from './components/Dashboard';
 import './App.css';
 import CocktailDropdown from './components/CocktailDropdown';
-//import FlowerIntegralGraph from './FlowerIntegralGraph';
+//import FlowerIntegralGraph from './components/FlowerIntegralGraph';
 
 import BettingScreen from './components/BettingScreen';
 import GpsMapComponent from './components/GpsMapComponent';
@@ -43,6 +43,7 @@ function App() {
         </p>
 
         <DateDisplay />
+        
       
         <DerivativeGraph />
 
